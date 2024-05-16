949e08155e5e8cfb8c7743059c3ba66d3a62407d
Emmanuel <manumtz20@gmail.com>
2024-05-16 14:16:49 -0600

    Agregar archivos de proyecto.

---
+++ 01-HelloWorld/Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'mostrar un mensaje por consola
+        Console.WriteLine("Hello World!!")
+
+        'permite poner en pausa el programa hasta que se presione una tecla
+        Console.ReadKey(True)
+
+        'podemos omitir "Console.ReadKey(True)" si iniciamos el programa sin depurar 
+
+    End Sub
+
+End Module
